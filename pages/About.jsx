bfcdefe5c1f5e4262fc4e96ecc453963ede66a14
DanielSahar <daniel.sahar@cycognito.com>
2026-02-26 17:10:29 +0200

    Push a fix for github pages

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,7 +7,7 @@ export function About() {
     return <section className="about">
         <h1 ref={titleRef}>About cars and us...</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio dolore sapiente, iste animi corporis nisi atque tempora assumenda dolores. Nobis nam dolorem rerum illo facilis nemo sit voluptatibus laboriosam necessitatibus!</p>
-
+        <p>test for github</p>
         <button onClick={() => {
             utilService.animateCSS(titleRef.current)
         }}>Animate</button>
